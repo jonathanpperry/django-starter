673e5c8517fb34349754ee04037f21e786fc6f8b
Jonathan Perry <jpper@umich.edu>
2019-08-14 17:53:23 +0900

    Added some form validation

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,18 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your description",
+                "class": "new-class-name two",
+                "id": "this-id",
+                "rows": 10,
+                "cols": 150
+            }))
+    price = forms.DecimalField()
     class Meta:
         model = Product
         fields = [
@@ -12,6 +24,14 @@ class ProductForm(forms.ModelForm):
         ]
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your description"}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "class": "new-class-name two",
+                "id": "this-id",
+                "rows": 10,
+                "cols": 70
+            }))
     price = forms.DecimalField()
--- src/products/views.py
+++ src/products/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 from .forms import ProductForm, RawProductForm
 from .models import Product
 
+def render_initial_data(request):
+    initial_data = {
+        'title': "My awesome title"
+    }
+    obj = Product.objects.get(id=1)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 def product_create_view(request):
     my_form = RawProductForm()
