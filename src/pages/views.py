f4f41fed4864f63c2c4cb386551f77b00b90e21a
Jonathan Perry <jpper@umich.edu>
2019-08-14 13:37:14 +0900

    Changed views.py to use templating instead of the HttpResponse

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,10 +5,10 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
 
-def about_view(*args, **kwargs):
-    return HttpResponse("<h1>About Me</h1>")
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
